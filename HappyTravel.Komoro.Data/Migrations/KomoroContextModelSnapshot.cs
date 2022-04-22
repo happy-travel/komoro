502e43c057fefd548a3d63134004f14470577ead
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-04-23 01:15:06 +0600

    Fixed migration

--- HappyTravel.Komoro.Data/Migrations/KomoroContextModelSnapshot.cs
+++ HappyTravel.Komoro.Data/Migrations/KomoroContextModelSnapshot.cs
@@ -174,7 +174,7 @@ namespace HappyTravel.Komoro.Data.Migrations
 
                     NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
 
-                    b.Property<Address>("Address")
+                    b.Property<Models.Statics.Address>("Address")
                         .IsRequired()
                         .HasColumnType("jsonb");
 
