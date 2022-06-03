52eb1fce5cfdb3297ae506651088dc58d9d993fc
Denis Nikolaev <denis.nikolaev@happytravel.tech>
2022-06-03 07:52:31 +0500

    added identity settings

---
+++ HappyTravel.Komoro.Api/Infrastructure/Options/AuthorityOptions.cs
@@ -0,0 +1,8 @@
+namespace HappyTravel.Komoro.Api.Infrastructure.Options;
+
+public class AuthorityOptions
+{
+    public string? AuthorityUrl { get; set; }
+    public string? Audience { get; set; }
+    public TimeSpan AutomaticRefreshInterval { get; set; }
+}
