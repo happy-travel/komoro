ff9bd9cd3f76ec9911384ba3941b4e65e9eff1fd
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-07-15 04:04:44 +0600

    Some fixes

--- HappyTravel.Komoro.Api/Services/Availabilities/WideAvailabilitySearchService.cs
+++ HappyTravel.Komoro.Api/Services/Availabilities/WideAvailabilitySearchService.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpFunctionalExtensions;
 using HappyTravel.EdoContracts.Accommodations;
+using HappyTravel.EdoContracts.Accommodations.Internals;
+using HappyTravel.Komoro.Api.Services.Converters;
 using HappyTravel.Komoro.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,28 +17,40 @@ public class WideAvailabilitySearchService : IWideAvailabilitySearchService
 
     public async Task<Result<Availability>> Get(string supplierCode, AvailabilityRequest request, CancellationToken cancellationToken)
     {
-        foreach (var roomOccupationRequest in request.Rooms)
+        foreach (var propertyCode in request.AccommodationIds)
         {
-            foreach (var propertyCode in request.AccommodationIds)
+            List<RoomContract> roomContracts = new(0);
+            foreach (var roomOccupationRequest in request.Rooms)
             {
                 var rooms = await _komoroContext.Rooms.Include(r => r.Property)
-                    .Where(r => r.Property.SupplierCode == supplierCode && r.Property.Code == propertyCode)
+                    .Where(r => r.Property.SupplierCode == supplierCode && r.Property.Code == propertyCode
+                        && r.MaximumOccupancy.Any(o => o.Adults >= roomOccupationRequest.AdultsNumber && o.Children >= roomOccupationRequest.ChildrenAges.Count))
                     .ToListAsync(cancellationToken);
 
                 var inventories = await _komoroContext.Inventories.Include(i => i.Property)
                     .Include(i => i.RoomType)
                     .Where(i => i.Property.SupplierCode == supplierCode && i.Property.Code == propertyCode
-                        && i.RoomType.
-                        && i.StartDate <= DateOnly.FromDateTime(request.CheckInDate.Date)   // Need to clarify
-                        && i.EndDate >= DateOnly.FromDateTime(request.CheckOutDate.Date)    // Need to clarify
+                        && rooms.Any(r => r.RoomType == i.RoomType)
+                        && i.StartDate <= DateOnly.FromDateTime(request.CheckInDate.Date)   // Need to clarify after receiving data from the supplier
+                        && i.EndDate >= DateOnly.FromDateTime(request.CheckOutDate.Date)    // Need to clarify after receiving data from the supplier
                         && i.NumberOfAvailableRooms > 0)
                     .ToListAsync(cancellationToken);
+
+                var availabilityRestrictions = await _komoroContext.AvailabilityRestrictions.Include(i => i.Property)
+                    .Where(ar => ar.Property.SupplierCode == supplierCode && ar.Property.Code == propertyCode)
+                    .ToListAsync(cancellationToken);
+
+                roomContracts = inventories.Select(i => WideAvailabilitySearchConverter.Convert(i, roomOccupationRequest)).ToList();
             }
-
+            var roomContractSet = new RoomContractSet(id: Guid.NewGuid(),
+                rate: new EdoContracts.General.Rate(),  // Need clarify
+                deadline: new Deadline(),   // Need clarify
+                rooms: roomContracts,
+                tags: new List<string>(),
+                isDirectContract: false,
+                isAdvancePurchaseRate: false,
+                isPackageRate: false);
         }
-
-
-
 
         throw new NotImplementedException();
     }
---
+++ HappyTravel.Komoro.Api/Services/Converters/WideAvailabilitySearchConverter.cs
@@ -0,0 +1,44 @@
+﻿using HappyTravel.EdoContracts.Accommodations.Enums;
+using HappyTravel.EdoContracts.Accommodations.Internals;
+using HappyTravel.EdoContracts.General;
+using HappyTravel.Komoro.Data.Models.Availabilities;
+
+namespace HappyTravel.Komoro.Api.Services.Converters
+{
+    public class WideAvailabilitySearchConverter
+    {
+        public static RoomContract Convert(Inventory inventory, RoomOccupationRequest request)
+        {
+            return new RoomContract(boardBasis: GetBoardBasis(inventory.RatePlanCode),
+                mealPlan: "",   // Need clarify
+                contractTypeCode: 0,    // Need clarify
+                isAvailableImmediately: true,
+                isDynamic: false,
+                contractDescription: "",    // Need clarify
+                remarks: new List<KeyValuePair<string, string>>(),
+                dailyRoomRates: new List<DailyRate>(),  // Need clarify
+                rate: new EdoContracts.General.Rate(),  // Need clarify
+                adultsNumber: request.AdultsNumber,
+                childrenAges: request.ChildrenAges, 
+                type: request.Type,
+                isExtraBedNeeded: request.IsExtraBedNeeded,
+                deadline: default,  // Need clarify
+                isAdvancePurchaseRate: false);
+        }
+
+
+        private static BoardBasisTypes GetBoardBasis(string ratePlanCode)
+            => ratePlanCode switch
+            {
+                "StandardRO" => BoardBasisTypes.RoomOnly,
+                "StandardBB" => BoardBasisTypes.BedAndBreakfast,
+                "StaySaveRO" => BoardBasisTypes.RoomOnly,
+                "StaySaveBB" => BoardBasisTypes.BedAndBreakfast,
+                "EarlyBirdRO" => BoardBasisTypes.RoomOnly,
+                "EarlyBirdBB" => BoardBasisTypes.BedAndBreakfast,
+                "SpecialDealRO" => BoardBasisTypes.RoomOnly,
+                "SpecialDealBB" => BoardBasisTypes.BedAndBreakfast,
+                _ => throw new NotImplementedException("Uncorrect rateplan code")
+            };
+    }
+}
